a2ec8b984367d86d7a30b5c0e71e8927e83866dd
Omar Valenzuela <ovalenzuela@MacBook-Pro-de-Omar.local>
2016-06-13 11:30:20 -0400

    prueba tres

---
+++ src/main/java/cl/curso/java/prueba3/prueba3/App.java
@@ -0,0 +1,13 @@
+package cl.curso.java.prueba3.prueba3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
